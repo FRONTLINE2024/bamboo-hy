e8231a76c63faec538b592a1138534864b4eb70a
yoona1110 <gjdbsdk18@icloud.com>
2024-09-28 16:55:34 +0900

    feat: header 컴포넌트 구현

---
+++ src/common/components/button.js
@@ -0,0 +1,4 @@
+function button() {
+    return `<div>btn</div>`
+}
+export default button;
--- src/common/components/header.js
+++ src/common/components/header.js
@@ -1,6 +1,30 @@
+// import Button from "./button";
+import './style.css';
+
 function header() {
+    let signUp = false;
+
+    const signUpCheck = signUp ? `
+        <div class='header-right'>
+            <p>하리미</p>
+            <p>님</p>
+        </div>
+    ` : ` 
+        <div class='header-right'>
+            <input type='text' placeholder='아이디'/>
+            <input type='password' placeholder='비밀번호'/>
+            <button>로그인</button>
+            <button>회원가입</button>
+        </div>`;
+    
     return `
-        <div>header</div>
-    `
+        <header>
+            <div class='header-left'>
+                <p>림대</p>
+                <p>SOOP</p>
+            </div>
+            ${signUpCheck}
+        </header>
+    `;
 }
 export default header;
