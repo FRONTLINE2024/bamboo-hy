77738c39c6edb31d044d42357bc30e62f375a8d7
yoona1110 <gjdbsdk18@icloud.com>
2024-10-01 20:19:31 +0900

    feat: writingItem 구현

--- src/features/feed/index.js
+++ src/features/feed/index.js
@@ -1,8 +1,10 @@
 import writingButton from "../../common/components/writingButton";
+import writingItem from "../../common/components/writingItem";
 
 function feed() {
     return `
         <div>feed</div>
+        ${writingItem({content: 'test', image: [], nickname: '한리미', date: '2024-10-01', isWrite: false})}
         ${writingButton()}
     `;
 }
