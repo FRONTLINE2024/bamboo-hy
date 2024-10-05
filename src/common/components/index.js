f168c8f4bbd0c5d0f1dfc61f002332e1c3e43c48
yoona1110 <gjdbsdk18@icloud.com>
2024-10-05 23:03:42 +0900

    refactor: component 경로 정리

---
+++ src/common/components/index.js
@@ -0,0 +1,4 @@
+import writingButton from './writingButton';
+import writingItem from './writingItem';
+
+export default { writingButton, writingItem };
--- src/features/feed/index.js
+++ src/features/feed/index.js
@@ -1,11 +1,10 @@
-import writingButton from "../../common/components/writingButton";
-import writingItem from "../../common/components/writingItem";
+import component from '../../common/components';
 
 function feed() {
     return `
         <div>feed</div>
-        ${writingItem({content: 'test', image: [], nickname: '한리미', date: '2024-10-01', isWrite: false})}
-        ${writingButton()}
+        ${component.writingItem({content: 'test', image: [], nickname: '한리미', date: '2024-10-01', isWrite: false})}
+        ${component.writingButton()}
     `;
 }
 
